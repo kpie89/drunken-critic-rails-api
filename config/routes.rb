19b16ca11446cedc6252ec701aca4bc565621e15
kpie89 <kpie89@gmail.com>
2016-02-25 17:29:05 -0500

    update beers routes. all beer, breweries and style

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :ratings, except: [:new, :edit]
+  resources :beers, except: [:new, :edit]
+  get 'hops' => 'beers#hops'
+  get '/styles' => 'beers#styles'
+  get '/categories' => 'beers#categories'
+  get '/breweries' => 'beers#breweries'
+  get '/beer/random' => 'beers#random_beer'
+  get '/brewery/random' => 'beers#random_brewery'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
