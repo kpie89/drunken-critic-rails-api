af944fdaabfa9df764cb42672e3efe6b692dce15
kpie89 <kpie89@gmail.com>
2016-02-25 16:58:17 -0500

    update for GET all beers

---
+++ app/models/beer.rb
@@ -0,0 +1,24 @@
+# class Beer
+#   def self.all
+#     HTTParty.get('http://api.brewerydb.com/v2/beers/?key=ENV['BREWERY_DB_KEY']')
+#   end
+# end
+
+class Beer
+  def self.all
+    HTTParty.get('http://api.brewerydb.com/v2/beers/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+  end
+
+  def self.random_beer
+    HTTParty.get('http://api.brewerydb.com/v2/beer/random/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+  end
+
+  def self.breweries
+    HTTParty.get('http://api.brewerydb.com/v2/breweries/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+  end
+
+  def self.random_brewery
+    HTTParty.get('http://api.brewerydb.com/v2/brewery/random/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+  end
+
+end
