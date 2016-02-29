ab4d39aa58dcd3b6cd63f279c0d4a89a3dd10fe5
kpie89 <kpie89@gmail.com>
2016-02-29 17:21:17 -0500

    final schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724141255) do
+ActiveRecord::Schema.define(version: 20160226200459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "score"
+    t.text     "desc"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "beer_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
