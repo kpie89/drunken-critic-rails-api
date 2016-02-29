495185091874ae75c335436185a5f36ba790a0bb
kpie89 <kpie89@gmail.com>
2016-02-29 17:19:46 -0500

    update route for search by beer

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   resources :ratings
   resources :beers, except: [:new, :edit]
-  get 'hops' => 'beers#hops'
-  get '/styles' => 'beers#styles'
-  get '/categories' => 'beers#categories'
-  get '/breweries' => 'beers#breweries'
-  get '/beer/random' => 'beers#random_beer'
+  get '/beer/rand' => 'beers#random_beer'
   get '/brewery/random' => 'beers#random_brewery'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
+  get '/beer' => 'beers#search_by_key'
   resources :users, only: [:index, :show]
 end
