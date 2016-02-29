b033c41e3d2e36efb73850d37a382bbc70f0f440
kpie89 <kpie89@gmail.com>
2016-02-29 17:16:13 -0500

    finalize beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,36 +1,22 @@
-# class Beer
-#   def self.all
-#     HTTParty.get('http://api.brewerydb.com/v2/beers/?key=ENV['BREWERY_DB_KEY']')
-#   end
-# end
+class Beer < ActiveRecord::Base
+  has_many :ratings, inverse_of: :beer
 
-class Beer
-  def self.all
-    HTTParty.get('http://api.brewerydb.com/v2/beers/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+
+  def self.search_by_key(key)
+    if key
+      response = HTTParty.get("http://prost.herokuapp.com/api/v1/beer/#{key}")
+      {beer: response['title'], key: response['key'], abv: response['abv'], style: response['tags'], brewery: response['brewery']}
+    end
   end
 
   def self.random_beer
-    HTTParty.get('http://api.brewerydb.com/v2/beer/random/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+    response = HTTParty.get('http://prost.herokuapp.com/api/v1/beer/rand')
+    {beer: response['title'], key: response['key'], abv: response['abv'], style: response['tags'], brewery: response['brewery']}
   end
 
-  def self.breweries
-    HTTParty.get('http://api.brewerydb.com/v2/breweries/?key=d94be20c4ddc36bf7de6ea0b615393a1')
-  end
-
-  def self.random_brewery
-    HTTParty.get('http://api.brewerydb.com/v2/brewery/random/?key=d94be20c4ddc36bf7de6ea0b615393a1')
-  end
-
-  def self.categories
-    HTTParty.get('http://api.brewerydb.com/v2/categories/?key=d94be20c4ddc36bf7de6ea0b615393a1')
-  end
-
-  def self.styles
-    HTTParty.get('http://api.brewerydb.com/v2/styles/?key=d94be20c4ddc36bf7de6ea0b615393a1')
-  end
-
-  def self.hops
-    HTTParty.get('http://api.brewerydb.com/v2/hops/?key=d94be20c4ddc36bf7de6ea0b615393a1')
+  def self.find(id)
+    response = HTTParty.get("http://prost.herokuapp.com/api/v1/beer/#{key}")
+    {beer: response['title'], key: response['key'], abv: response['abv'], style: response['tags'], brewery: response['brewery']}
   end
 
 end
